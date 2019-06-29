bf4b18467bfe783eed64ee744a31185d2a626b5e
Pranav Raut <pranavraut033@gmail.com>
2019-06-29 22:33:25 +0530

    added logger, cookie, cors and session middleware
    Signed-off-by: Pranav Raut <pranavraut033@gmail.com>

--- src/index.js
+++ src/index.js
@@ -1,5 +1,9 @@
 import express from 'express';
 import routes from './routes/routes';
+import session from 'express-session';
+import cookieParser from 'cookie-parser';
+import logger from 'morgan';
+import cors from 'cors';
 import {
 	urlencoded,
 	json
@@ -11,14 +15,36 @@ import {
 } from "./config/database.config";
 import config from './config/index.js';
 
+const MongoStore = require('connect-mongo')(session);
 
 const app = express();
 
+app.use(logger('dev'));
+
+app.use(cookieParser());
 app.use(json());
 app.use(urlencoded({
 	extended: true
 }));
 
+
+app.set('trust proxy', 1) // trust first proxy
+app.use(session({
+	store: new MongoStore({
+		mongooseConnection: mongoose.connection
+	}),
+	resave: true,
+	saveUninitialized: true,
+	secret: 'keyboard cat',
+	cookie: {
+		maxAge: 2 * 60 * 60 * 100
+	}
+}))
+
+app.use(cors({
+	credentials: true,
+	origin: true
+}));
 routes(app);
 // listen for requests
 app.listen(config.port, () => {
