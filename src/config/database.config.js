6070879dd13b4ff8d9f86164b9f332799b7337ce
Pranav Raut <pranavraut033@gmail.com>
2019-06-29 22:30:46 +0530

    Moved mongoose conntection to database.config.js
    Signed-off-by: Pranav Raut <pranavraut033@gmail.com>

--- src/config/database.config.js
+++ src/config/database.config.js
@@ -1,3 +1,18 @@
-const url = 'mongodb://localhost:27017/clinic-database';
+var m = require('mongoose');
 
-export default url;
+export const url = 'mongodb://localhost:27017/clinic-database';
+
+m.Promise = global.Promise;
+// FIX for: DeprecationWarning: collection.ensureIndex is deprecated. Use createIndexes instead.
+m.set('useCreateIndex', true);
+// Connecting to the database
+m.connect(url, {
+    useNewUrlParser: true
+}).then(() => {
+    console.log("Successfully connected to the database: ", url);
+}).catch(err => {
+    console.error('Could not connect to the database[' + url + ']. Exiting now...', err);
+    process.exit();
+});
+
+export const mongoose = m;
--- src/index.js
+++ src/index.js
@@ -1,41 +1,23 @@
 import express from 'express';
+import routes from './routes/routes';
 import {
 	urlencoded,
 	json
 } from 'body-parser';
 
-import url from './config/database.config.js';
-import routes from './routes/routes.js';
+import {
+	mongoose
+} from "./config/database.config";
 
-import _ from "./utils/init.js"
-
-const mongoose = require('mongoose');
-mongoose.Promise = global.Promise;
-
-// Connecting to the database
-mongoose.connect(url, {
-	useNewUrlParser: true
-}).then(() => {
-	console.log("Successfully connected to the database: ", url);
-}).catch(err => {
-	console.error('Could not connect to the database[' + url + ']. Exiting now...', err);
-	process.exit();
-});
 
 const app = express();
 
-// parse requests of content-type - application/x-www-form-urlencoded
+app.use(json());
 app.use(urlencoded({
 	extended: true
 }));
 
-
-
-// parse requests of content-type - application/json
-app.use(json());
-
-routes(app); // assign all the routes to the app
-
+routes(app);
 // listen for requests
 app.listen(3000, () => {
 	console.log("Server is listening on port 3000");
